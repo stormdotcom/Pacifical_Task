405077ca7df13f678f5ab59d05ed818543eda0c0
Ajmal Nasumudeen <ajmaln73@gmail.com>
2024-09-30 13:49:52 +0530

    project structure

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+// const db = require('./utils/db');
+
+
+dotenv.config();
+
+
+app.use(express.json());
+
+
+
+app.get("/test",((req, res)=> {
+    res.status(200).json({message:"test"})
+})
+)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
