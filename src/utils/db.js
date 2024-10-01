d8a2466c675dec563e92ac62c88e47ff35d19a4f
Ajmal Nasumudeen <ajmaln73@gmail.com>
2024-10-01 12:24:51 +0530

    fix: docker container established pg and app

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,10 +3,17 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_URL, {
-  dialect: 'postgres',
-  logging:false
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME, // Database name
+  process.env.DB_USER, // Database username
+  process.env.DB_PASSWORD, // Database password
+  {
+    host: process.env.DB_HOST, // Database host (e.g., 'db' for Docker service name)
+    port: process.env.DB_PORT, // Database port (usually 5432 for Postgres)
+    dialect: 'postgres',
+    logging: false,
+  }
+);
 
 
 
